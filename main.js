435fc42078b02856b700b45632c799e0e3add595
paul <pauledworthy@gmail.com>
2022-01-28 06:26:12 -0500

    add keyboard using Javascript

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const board = document.querySelector('.game__container')
+const keyboard = document.querySelector('.game__keyboard')
+const keys = Array.from(document.querySelectorAll('.key'))
 
 const secretWord = 'guess'
 const wordLength = 5;
 const rows = 6
-const columns = 5
+const columns = wordLength
 let guesses = 0
 let currentWord = ''
 let wordsGuessed = []
@@ -37,8 +39,37 @@ document.addEventListener('keyup', (e) => {
   }
 })
 
-function checkWinner() {
+function updateBoard() {
+  let tile = boardRow.firstChild
+  
+  for (let i = 0; i < currentWord.length; i++) {
+    tile.textContent = currentWord[i]
+    updateBackground(tile, i)
+    tile = tile.nextSibling
+  }
+}
 
+function updateKeyboard(letter) {
+  keys.forEach( key => {
+  })
+}
+// if ((key.textContent.toLowerCase() === letter) && (currentWord.includes(letter))) {
+//   key.classList.add('misplaced')
+// }
+
+function updateBackground(tile, i) {
+  if(secretWord[i] === currentWord[i]) {
+    tile.classList.add('correct')
+  } else if (secretWord.includes(currentWord[i])){
+    tile.classList.add('misplaced')
+  } else {
+    tile.classList.add('wrong')
+  }
+  // updateKeyboard()
+  tile.style.border = 'none'
+}
+
+function checkWinner() {
   if (currentWord.length === secretWord.length) {
     if (currentWord === secretWord) {
       alert('WINNER!')
@@ -50,29 +81,28 @@ function checkWinner() {
   if (wordsGuessed.length === 6) {
     alert('GAME OVER')
   }
-  
+
   boardRow = boardRow.nextSibling
 }
 
-function updateBoard() {
+// Build the keyboard
+const firstRow = 'qwertyuiop'
+const secondRow = 'asdfghjkl'
+const thirdRow = 'zxcvbnm'
+buildKeyboard(firstRow)
+buildKeyboard(secondRow)
+buildKeyboard(thirdRow)
 
-  let tile = boardRow.firstChild
-  
-  for (let i = 0; i < currentWord.length; i++) {
-    tile.textContent = currentWord[i]
-    updateBackground(tile, i)
-    tile = tile.nextSibling
+function buildKeyboard(keyboardRow) {
+
+  let row = document.createElement('div')
+  row.className = 'keyboard__row'
+
+  for (let j = 0; j < keyboardRow.length; j++) {
+    let key = document.createElement('div')
+    key.className = 'key'
+    key.textContent = keyboardRow[j]
+    row.appendChild(key)
   }
-}
-
-function updateBackground(tile, i) {
-
-  if(secretWord[i] === currentWord[i]) {
-    tile.classList.add('correct')
-  } else if (secretWord.includes(currentWord[i])){
-    tile.classList.add('misplaced')
-  } else {
-    tile.classList.add('wrong')
-  }
-  tile.style.border = 'none'
+  keyboard.appendChild(row)
 }
